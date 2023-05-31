711c85246ffdecef9809d4ea7e71bdae3e2bb48b
Anish Kanthamneni <akneni@gmail.com>
2023-05-31 01:43:23 -0400

    Update setup.py file

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
     name='pygrab',
-    version='0.0.2',
+    version='0.1.0',
     description='A secure python library for fetching data with async support',
     author='Anish Kanthamneni',
     author_email='akneni@gmail.com',
     packages=['pygrab'],
     install_requires=[
         'requests',
-        'beautifulsoup4',
+        'pyppeteer',
     ],
 )
