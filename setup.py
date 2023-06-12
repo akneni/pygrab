b16ce5ee47e7ff009d5d5afa56d43423384bce71
Anish Kanthamneni <akneni@gmail.com>
2023-06-12 01:42:10 -0400

    Update the version in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pygrab',
-    version='0.1.0',
+    version='0.1.1',
     description='A secure python library for fetching data with async support',
     author='Anish Kanthamneni',
     author_email='akneni@gmail.com',
