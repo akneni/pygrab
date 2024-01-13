dacd9e25d0633869c3a893b11f364956c5eca0c3
Anish Kanthamneni <akneni@gmail.com>
2024-01-13 14:35:23 -0500

    Add MANIFEST.in file to help with rust binary distribution

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
     author='Anish Kanthamneni',
     author_email='akneni@gmail.com',
     packages=find_packages(),
+    include_package_data=True,
     install_requires=[
         'requests',
         'pyppeteer',
