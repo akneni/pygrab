3d29681a5e3a958e0dc8bf243bf935792215410d
Anish Kanthamneni <109305222+akneni@users.noreply.github.com>
2024-07-25 14:01:57 -0400

    Update PyO3 dependency

---
+++ rust_sourcecode/src/lib.rs
@@ -0,0 +1,17 @@
+mod response;
+mod async_session;
+mod thread_session;
+
+use response::HttpResponse;
+use async_session::AsyncSessionRs;
+use thread_session::ThreadSessionRs;
+use pyo3::prelude::*;
+
+/// A Python module implemented in Rust.
+#[pymodule]
+fn rust_lib(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_class::<AsyncSessionRs>()?;
+    m.add_class::<ThreadSessionRs>()?;
+    m.add_class::<HttpResponse>()?;
+    Ok(())
+}
